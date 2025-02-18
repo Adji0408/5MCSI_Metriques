7f0a9ace541170717fdd8542d653e55acac41f25
Adji0408 <adji-fatou.drame@ism.edu.sn>
2025-02-18 15:09:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
+from flask import Flask, render_template, jsonify
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
-  
+    return render_template('hello.html')  #Com2
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
